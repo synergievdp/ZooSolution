11cb799bd46968b2a4e54ead9e81ee180d2e9ac9
Synergie van der Pluijm <synergievdp@gmail.com>
2021-05-14 11:16:03 +0200

    Add ValidationRule

---
+++ ZooWPFApp/PositiveIntegerValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ZooWPFApp {
+    class PositiveIntegerValidator : ValidationRule {
+            public override ValidationResult Validate(object value, CultureInfo cultureInfo) {
+                if ((value as string).All(c => char.IsDigit(c))) {
+                    return ValidationResult.ValidResult;
+                } else
+                    return new ValidationResult(false, "Only digits allowed");
+            }
+    }
+}
